48991cce60b4301a491806a3260f1e674a814803
“Micheal” <1061100739@qq.com>
2017-10-14 23:53:29 +0800

    10/14

--- app/src/main/java/cn/edu/gdmec/android/mobileguard/m1home/HomeActivity.java
+++ app/src/main/java/cn/edu/gdmec/android/mobileguard/m1home/HomeActivity.java
@@ -16,6 +16,7 @@ import cn.edu.gdmec.android.mobileguard.m1home.adapter.HomeAdapter;
 import cn.edu.gdmec.android.mobileguard.m2theftguard.dialog.InterPasswordDialog;
 import cn.edu.gdmec.android.mobileguard.m2theftguard.dialog.SetupPasswordDialog;
 
+import cn.edu.gdmec.android.mobileguard.m2theftguard.utils.LostFindActivity;
 import cn.edu.gdmec.android.mobileguard.m2theftguard.utils.MD5Utils;
 
 
@@ -117,6 +118,7 @@ public class HomeActivity extends AppCompatActivity {
                 }else if (password.equals(MD5Utils.encode(mInPswdDialog
                         .getPassword()))){
                     mInPswdDialog.dismiss();
+                    startActivity(LostFindActivity.class);
                     Toast.makeText(HomeActivity.this,"可以进入手机防盗模块",Toast.LENGTH_LONG).show();
                 }else {
                     mInPswdDialog.dismiss();
