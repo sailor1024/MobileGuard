32fbbf862c969f715ee53ef54b2c3dfec2c0068a
“Micheal” <1061100739@qq.com>
2017-10-21 18:20:22 +0800

    10-21

---
+++ app/src/main/java/cn/edu/gdmec/android/mobileguard/App.java
@@ -0,0 +1,45 @@
+package cn.edu.gdmec.android.mobileguard;
+
+import android.app.Application;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.telecom.TelecomManager;
+import android.telephony.SmsManager;
+import android.telephony.TelephonyManager;
+import android.text.TextUtils;
+import android.util.Log;
+public class App extends Application {
+    @Override
+    public void onCreate(){
+        super.onCreate();
+        correctSIM();
+    }
+
+    public void correctSIM() {
+        // 检查sim卡是否发生变化
+        SharedPreferences sp = getSharedPreferences("config",
+                Context.MODE_PRIVATE);
+
+        boolean protecting = sp.getBoolean("protecting", true);
+        if (protecting) {
+            String bindsim = sp.getString("sim", "");
+
+            TelephonyManager tm = (TelephonyManager) getSystemService(Context.TELEPHONY_SERVICE);
+
+            String realsim = tm.getSimSerialNumber();
+            realsim = "999";
+            if (bindsim.equals(realsim)) {
+                Log.i("", "sim卡未发生变化，还是你的手机");
+            } else {
+                Log.i("", "SIM卡变化了");
+
+                String safenumber = sp.getString("safephone", "");
+                if (!TextUtils.isEmpty(safenumber)) {
+                    SmsManager smsManager = SmsManager.getDefault();
+                    smsManager.sendTextMessage(safenumber, null,
+                            "你的亲友手机的SIM卡已经被更换！", null, null);
+                }
+            }
+        }
+    }
+}
---
+++ app/src/main/java/cn/edu/gdmec/android/mobileguard/m2theftguard/entity/ContactInfo.java
@@ -0,0 +1,11 @@
+package cn.edu.gdmec.android.mobileguard.m2theftguard.entity;
+
+/**
+ * Created by 郑竣鸿 on 2017/10/19.
+ */
+
+public class ContactInfo {
+    public String id;
+    public String name;
+    public String phone;
+}
