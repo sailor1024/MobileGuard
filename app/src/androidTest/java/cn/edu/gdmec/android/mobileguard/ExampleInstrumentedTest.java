f42a67a610287949f5506488c77180f208470613
sailor1024 <1663852293@qq.com>
2017-11-05 20:25:35 +0800

    1105 15:29

--- app/src/androidTest/java/cn/edu/gdmec/android/mobileguard/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/cn/edu/gdmec/android/mobileguard/ExampleInstrumentedTest.java
@@ -1,5 +1,5 @@
 package cn.edu.gdmec.android.mobileguard;
-/*
+
 import android.content.Context;
 import android.content.Intent;
 import android.support.test.InstrumentationRegistry;
@@ -328,28 +328,4 @@ public class ExampleInstrumentedTest {
         String str = result.getText();
     }
 
-}*/
-import android.content.Context;
-import android.support.test.InstrumentationRegistry;
-import android.support.test.runner.AndroidJUnit4;
-
-import org.junit.Test;
-import org.junit.runner.RunWith;
-
-import static org.junit.Assert.*;
-
-/**
- * Instrumentation test, which will execute on an Android device.
- *
- * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
- */
-@RunWith(AndroidJUnit4.class)
-public class ExampleInstrumentedTest {
-    @Test
-    public void useAppContext() throws Exception {
-        // Context of the app under test.
-        Context appContext = InstrumentationRegistry.getTargetContext();
-
-        assertEquals("cn.edu.gdmec.android.moblieguard", appContext.getPackageName());
-    }
 }
