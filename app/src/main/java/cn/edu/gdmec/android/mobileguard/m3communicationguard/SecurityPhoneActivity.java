6e7bd4bdca6f1c006c99d446ee9200108b13c4b6
sailor1024 <1663852293@qq.com>
2017-11-05 20:14:12 +0800

    1105 15:29

--- app/src/main/java/cn/edu/gdmec/android/mobileguard/m3communicationguard/SecurityPhoneActivity.java
+++ app/src/main/java/cn/edu/gdmec/android/mobileguard/m3communicationguard/SecurityPhoneActivity.java
@@ -35,31 +35,31 @@ public class SecurityPhoneActivity extends AppCompatActivity implements View.OnC
     private FrameLayout mNoBlackNumber;//没有黑名单的布局
     private BlackNumberDao dao;
     private ListView mListView;
-    private int pagenumber=0;
-    private int pagesize=15;
+    private int pagenumber = 0;
+    private int pagesize = 15;
     private int totalNumber;
     private List<BlackContactInfo> pageBlackNumber = new ArrayList<BlackContactInfo>();
     private BlackContactAdapter adapter;
 
-    private void fillData(){
-        dao=new BlackNumberDao(SecurityPhoneActivity.this);
-        totalNumber=dao.getTotalNumber();
-        if (totalNumber==0){
+    private void fillData() {
+        dao = new BlackNumberDao(SecurityPhoneActivity.this);
+        totalNumber = dao.getTotalNumber();
+        if (totalNumber == 0) {
             //黑名单没有数据
             mHaveBlackNumber.setVisibility(View.GONE);
             mNoBlackNumber.setVisibility(View.VISIBLE);
-        }else if(totalNumber>0){
+        } else if (totalNumber > 0) {
             //有数据
             mHaveBlackNumber.setVisibility(View.VISIBLE);
             mNoBlackNumber.setVisibility(View.GONE);
-            pagenumber=0;
-            if (pageBlackNumber.size()>10){
+            pagenumber = 0;
+            if (pageBlackNumber.size() > 10) {
                 pageBlackNumber.clear();
             }
-            pagesize=totalNumber;//老师的坑在这里解决了 总数=pasesize
-            pageBlackNumber.addAll(dao.getPageBlackNumber(pagenumber,pagesize));
-            if (adapter==null){
-                adapter=new BlackContactAdapter(pageBlackNumber,
+            pagesize = totalNumber;//老师的坑在这里解决了 总数=pasesize
+            pageBlackNumber.addAll(dao.getPageBlackNumber(pagenumber, pagesize));
+            if (adapter == null) {
+                adapter = new BlackContactAdapter(pageBlackNumber,
                         SecurityPhoneActivity.this);
                 adapter.setCallBack(new BlackContactAdapter.BlackConactCallBack() {
                     @Override
@@ -68,41 +68,41 @@ public class SecurityPhoneActivity extends AppCompatActivity implements View.OnC
                     }
                 });
                 mListView.setAdapter(adapter);
-            }else {
+            } else {
                 adapter.notifyDataSetChanged();
             }
         }
     }
 
-    private void initView(){
+    private void initView() {
         findViewById(R.id.rl_titlebar).setBackgroundColor(
                 getResources().getColor(R.color.bright_purple));
-        final ImageView mLeftImgv = (ImageView)findViewById(R.id.imgv_leftbtn);
-        ((TextView)findViewById(R.id.tv_title)).setText("通讯卫士");
+        final ImageView mLeftImgv = (ImageView) findViewById(R.id.imgv_leftbtn);
+        ((TextView) findViewById(R.id.tv_title)).setText("通讯卫士");
         mLeftImgv.setOnClickListener(this);
         mLeftImgv.setImageResource(R.drawable.back);
-        mHaveBlackNumber=(FrameLayout)findViewById(R.id.fl_haveblacknumber);
-        mNoBlackNumber=(FrameLayout)findViewById(R.id.fl_noblacknumber);
+        mHaveBlackNumber = (FrameLayout) findViewById(R.id.fl_haveblacknumber);
+        mNoBlackNumber = (FrameLayout) findViewById(R.id.fl_noblacknumber);
         findViewById(R.id.btn_addblacknumber).setOnClickListener(this);
-        mListView=(ListView)findViewById(R.id.lv_blacknumbers);
-        mListView.setOnScrollListener(new AbsListView.OnScrollListener(){
+        mListView = (ListView) findViewById(R.id.lv_blacknumbers);
+        mListView.setOnScrollListener(new AbsListView.OnScrollListener() {
             @Override
             public void onScrollStateChanged(AbsListView absListView, int i) {
-                switch (i){//i是列表的滚动状态
+                switch (i) {//i是列表的滚动状态
                     //     case AbsListView.OnScrollListener.SCROLL_STATE_IDLE://列表滑动后静止
                     //     case AbsListView.OnScrollListener.SCROLL_STATE_TOUCH_SCROLL://手指正拖着滑动
                     case AbsListView.OnScrollListener.SCROLL_STATE_FLING://列表自由滑动
                         //获取最后一个可见条目
                         int lastVisiblePosition = mListView.getLastVisiblePosition();
-                        if (lastVisiblePosition==pageBlackNumber.size()-1){
+                        if (lastVisiblePosition == pageBlackNumber.size() - 1) {
                             //如果当前条目是最后一个,增查询更多的数据
                             pagenumber++;
 
-                            if (pagenumber*pagesize>=totalNumber){
-                                Toast.makeText(SecurityPhoneActivity.this,"没有更多数据了",Toast.LENGTH_LONG).show();
-                            }else {
+                            if (pagenumber * pagesize >= totalNumber) {
+                                Toast.makeText(SecurityPhoneActivity.this, "没有更多数据了", Toast.LENGTH_LONG).show();
+                            } else {
                                 pageBlackNumber.addAll(dao.getPageBlackNumber(
-                                        pagenumber,pagesize));
+                                        pagenumber, pagesize));
                                 adapter.notifyDataSetChanged();
                             }
                         }
@@ -129,13 +129,13 @@ public class SecurityPhoneActivity extends AppCompatActivity implements View.OnC
 
     @Override
     public void onClick(View view) {
-        switch (view.getId()){
+        switch (view.getId()) {
             case R.id.imgv_leftbtn:
                 finish();
                 break;
             case R.id.btn_addblacknumber:
                 //跳转至添加黑名单页面
-                startActivity(new Intent(this,AddBlackNumberActivity.class));
+                startActivity(new Intent(this, AddBlackNumberActivity.class));
                 break;
         }
     }
@@ -143,18 +143,20 @@ public class SecurityPhoneActivity extends AppCompatActivity implements View.OnC
     @Override
     protected void onRestart() {
         super.onRestart();
-        if (dao.getTotalNumber()>0){
-            mHaveBlackNumber.setVisibility(View.VISIBLE);
-            mNoBlackNumber.setVisibility(View.GONE);
-        }else {
-            mHaveBlackNumber.setVisibility(View.GONE);
-            mNoBlackNumber.setVisibility(View.VISIBLE);
-        }
-        pagenumber=0;
-        pageBlackNumber.clear();
-        pageBlackNumber.addAll(dao.getPageBlackNumber(pagenumber,pagesize));
-        if (adapter!=null){
-            adapter.notifyDataSetChanged();
+        if (totalNumber != dao.getTotalNumber()) {
+            if (dao.getTotalNumber() > 0) {
+                mHaveBlackNumber.setVisibility(View.VISIBLE);
+                mNoBlackNumber.setVisibility(View.GONE);
+            } else {
+                mHaveBlackNumber.setVisibility(View.GONE);
+                mNoBlackNumber.setVisibility(View.VISIBLE);
+            }
+            pagenumber = 0;
+            pageBlackNumber.clear();
+            pageBlackNumber.addAll(dao.getPageBlackNumber(pagenumber, pagesize));
+            if (adapter != null) {
+                adapter.notifyDataSetChanged();
+            }
         }
     }
 }
