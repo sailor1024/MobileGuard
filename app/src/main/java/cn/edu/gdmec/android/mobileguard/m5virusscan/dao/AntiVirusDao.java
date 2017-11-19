ed1ac7de4da9ece3c71066981725c2a0db715573
“Micheal” <1061100739@qq.com>
2017-11-19 08:58:05 +0800

    手机杀毒

---
+++ app/src/main/java/cn/edu/gdmec/android/mobileguard/m5virusscan/dao/AntiVirusDao.java
@@ -0,0 +1,55 @@
+package cn.edu.gdmec.android.mobileguard.m5virusscan.dao;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+
+
+public class AntiVirusDao {
+
+
+    private static Context context;
+    private static String dbname;
+    private String md5;
+    private String desc;
+
+    public AntiVirusDao(Context context){
+        this.context = context;
+        dbname = "/data/data/"+context.getPackageName ()+"/files/antivirus.db";
+    }
+
+    public String checkVirus(String md5){
+        String desc = null;
+        SQLiteDatabase db = SQLiteDatabase.openDatabase (
+                dbname, null,
+                SQLiteDatabase.OPEN_READONLY );
+        Cursor cursor = db.rawQuery ( "select desc from datable where md5=?",
+                new String[] { md5 });
+        if (cursor.moveToNext ()){
+            desc = cursor.getString ( 0 );
+        }
+        cursor.close ();
+        db.close ();
+        return desc;
+    }
+
+    public String getVirusDbVersion(){
+        String dbVersion = null;
+
+        SQLiteDatabase db = SQLiteDatabase.openDatabase(
+                dbname, null,
+                SQLiteDatabase.OPEN_READONLY);
+
+        Cursor cursor = db.rawQuery("select desc from datable wheremd=?",new String[] {md5});
+
+        if (cursor.moveToNext()) {
+            dbVersion = cursor.getString(0);
+        }
+        cursor.close();
+        db.close();
+
+
+        
+        return desc;
+    }
+}
---
+++ app/src/main/java/cn/edu/gdmec/android/mobileguard/m5virusscan/entity/ScanAppInfo.java
@@ -0,0 +1,12 @@
+package cn.edu.gdmec.android.mobileguard.m5virusscan.entity;
+
+import android.graphics.drawable.Drawable;
+
+
+public class ScanAppInfo {
+    public String appName;
+    public boolean isVirus;
+    public String packagename;
+    public String description;
+    public Drawable appicon;
+}
