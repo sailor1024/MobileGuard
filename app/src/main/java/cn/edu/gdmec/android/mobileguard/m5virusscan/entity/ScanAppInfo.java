d464b75fc49fe5683f751f34b67135205209f4e2
sailor1024 <1663852293@qq.com>
2017-12-21 21:17:41 +0800

    m9-2--2

--- app/src/main/java/cn/edu/gdmec/android/mobileguard/m5virusscan/entity/ScanAppInfo.java
+++ app/src/main/java/cn/edu/gdmec/android/mobileguard/m5virusscan/entity/ScanAppInfo.java
@@ -2,11 +2,17 @@ package cn.edu.gdmec.android.mobileguard.m5virusscan.entity;
 
 import android.graphics.drawable.Drawable;
 
-
+/**
+ * Created by admin on 2017/11/13.
+ */
 public class ScanAppInfo {
     public String appName;
     public boolean isVirus;
     public String packagename;
     public String description;
     public Drawable appicon;
+
+    //   课堂练习2017.11.28
+    public String virusScanUrl;
+    public String md5info;
 }
